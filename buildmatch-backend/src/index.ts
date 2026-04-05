7fb9af086ee44ef82df46168535a90f7fe497086
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2026-04-05 05:51:46 -0400

    updates

--- buildmatch-backend/src/index.ts
+++ buildmatch-backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.json({ status: 'ok' });
+  res.json({ status: 'ok', message: "The Server is currenltly healthy" });
 });
 
 app.listen(PORT, () => {
