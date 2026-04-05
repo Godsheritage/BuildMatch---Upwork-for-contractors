4a5e2caed02d0a64db31462f0defa05ec5d9f1b7
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2026-04-05 05:33:00 -0400

    updates

--- buildmatch-backend/src/server.ts
+++ buildmatch-backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { registerReliabilityScoreJob } from './jobs/reliability-score.job';
 
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT || 3001 || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
